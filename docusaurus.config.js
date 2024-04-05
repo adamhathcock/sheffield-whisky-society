a17ba08e2f532d7dfebf03a73e099a0e8227fb1c
Adam Hathcock <adam@disputed.io>
2024-04-05 09:29:42 +0100

    update and test themes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,8 +2,8 @@
 // Note: type annotations allow type checking and IDEs autocompletion
 
 const {themes} = require('prism-react-renderer')
-const lightCodeTheme = themes.github;
-const darkCodeTheme = themes.dracula;
+const lightCodeTheme = themes.gruvboxMaterialDark;
+const darkCodeTheme = themes.vsDark;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
